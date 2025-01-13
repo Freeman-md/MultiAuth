2e721a149bf0d60707a5edd490782742810c3dc7
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-01-13 12:09:31 +0000

    Add JWT authentication and Microsoft Identity Web support

--- api/Program.cs
+++ api/Program.cs
@@ -1,7 +1,24 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApi(
+                    options =>
+                    {
+                        builder.Configuration.Bind("AzureAdB2C", options);
+                    },
+                    options =>
+                    {
+                        builder.Configuration.Bind("AzureAdB2C", options);
+                    }
+                );
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -10,8 +27,15 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
+
+    app.UseRouting();
+
+app.UseAuthentication();
+
+app.MapControllers();
 
 app.Run();
